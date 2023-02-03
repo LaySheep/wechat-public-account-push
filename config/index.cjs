fc309e88f93f09d76cfc32c24959e573635af2f3
LaySheep <66475057+LaySheep@users.noreply.github.com>
2023-02-03 14:41:11 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '罗宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ou5ZR6SrXIWTwbAEhJsvHmwivGz4',
+      id: 'ou5ZR6WD3aGpivuGDqeio-ABIEFk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'HInbRL2lwg1UB78tKqAsTEVvcM1zayAHJgTJhVYY1gI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
